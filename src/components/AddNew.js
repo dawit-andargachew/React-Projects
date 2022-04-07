3bfa9a85e27e510d5b57bf0e76462f1f6828d599
Dawit Andargachew <dawit1111111@gmail.com>
2022-04-07 14:48:22 +0300

    Commit 5, the user dummy data is sent to remote googles Firebase free api, and the user is redirected to products page           -> Addnew : uses <AddNewForm/> as JSX element and hanldes sending data to api and clearing inpu boxes           -> AddNewForm : containes the form and enables --AddNew-- to access dummy data and input boxs to clear after data is sent

--- src/components/AddNew.js
+++ src/components/AddNew.js
@@ -1,103 +1,38 @@
-import { useRef } from "react";
-import "./add_new.css";
+import AddNewForm from "./AddNewForm";
+import { useHistory } from "react-router-dom";
 
 function AddNew() {
-  const titleInputRef = useRef();
-  const imageInputRef = useRef();
-  const addressInputRef = useRef();
-  const descriptionInputRef = useRef();
+  const the_history = useHistory();
 
-  //submitHandler function
-  function submitHandler(event) {
-    /* removes the default event when submit which sends a request for the server, 
-    but by preventing the default react handles the process */
-    event.preventDefault();
+  /* it has access to 2 variables in the -> AddNewForm <- component
+        [data 1], the dummy data entered by the user, and
+        [data 2], the document.querySelectorAll() elements of all input boxs
+   */
+  function DataHandler(data, data_to_clear) {
+    // resolves [data 1]
+    fetch(
+      "https://react-getting-started-e0e8c-default-rtdb.firebaseio.com/Products.json", // google firebase api link
+      {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      // redirct to products page after data is entered
+      the_history.replace("/products");
 
-    // extracts user input
-    const enterdTitle = titleInputRef.current.value;
-    const entedImage = imageInputRef.current.value;
-    const enterdAdress = addressInputRef.current.value;
-    const enteredDescription = descriptionInputRef.current.value;
-
-    
-    const newData = {
-      title: enterdTitle,
-      image: entedImage,
-      address: enterdAdress,
-      description: enteredDescription,
-    };
-
-    console.log(newData);
-
-    /* helps to clear each input box after submit */
-    const inputs = document.querySelectorAll(".clear_after");
-    inputs.forEach((input) => {
-      input.value = "";
+      // resolves [data 2]
+      data_to_clear.forEach((one) => {
+        one.value = "";
+      });
     });
   }
 
   return (
     <div>
-      <div className="container">
-        <form onSubmit={submitHandler}>
-          {/* title */}
-          <div className="sinlge-input-part title">
-            <label htmlFor="title">Title</label>
-            <input
-              className="clear_after"
-              type="text"
-              required
-              id="title"
-              ref={titleInputRef}
-            />{" "}
-            <br />
-          </div>
-
-          {/* image */}
-          <div className="sinlge-input-part image">
-            <label htmlFor="image">Image</label>
-            <input
-              className="clear_after"
-              type="url"
-              required
-              id="image"
-              ref={imageInputRef}
-            />{" "}
-            <br />
-          </div>
-
-          {/* address */}
-          <div className="sinlge-input-part address">
-            <label htmlFor="address">Address</label>
-            <input
-              className="clear_after"
-              type="text"
-              required
-              id="address"
-              ref={addressInputRef}
-            />
-            <br />
-          </div>
-
-          {/* description */}
-          <div className="sinlge-input-part description">
-            <label htmlFor="description">Desctiption</label>
-            <textarea
-              className="clear_after"
-              name="description"
-              id="description"
-              required
-              rows={5}
-              ref={descriptionInputRef}
-            ></textarea>
-            <br />
-          </div>
-          {/* button */}
-          <div className="button-div">
-            <button className="button">Add New Product</button>
-          </div>
-        </form>
-      </div>
+      <AddNewForm clear_the_inputs_box={AddNew} Collected_data={DataHandler} />
     </div>
   );
 }
---
+++ src/components/AddNewForm.js
@@ -0,0 +1,104 @@
+import { useRef } from "react";
+import "./add_new.css";
+
+function AddNewForm(props,props__2) {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  //submitHandler function
+  function submitHandler(event) {
+    /* removes the default event when submit which sends a request for the server, 
+    but by preventing the default react handles the process */
+    event.preventDefault();
+
+    // extracts user input
+    const enterdTitle = titleInputRef.current.value;
+    const entedImage = imageInputRef.current.value;
+    const enterdAdress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const newData = {
+      title: enterdTitle,
+      image: entedImage,
+      address: enterdAdress,
+      description: enteredDescription,
+    };
+
+    // Make the -> newData <- accessible to send to backend in the ->"AddNew"<- component
+    props.Collected_data(newData);
+
+    /* helps to clear each input box after submit */
+    const inputs = document.querySelectorAll(".clear_after");
+    props__2.clear_the_inputs_box(inputs);
+
+  }
+
+  return (
+    <div>
+      <div className="container">
+        <form onSubmit={submitHandler}>
+          {/* title */}
+          <div className="sinlge-input-part title">
+            <label htmlFor="title">Title</label>
+            <input
+              className="clear_after"
+              type="text"
+              required
+              id="title"
+              ref={titleInputRef}
+            />{" "}
+            <br />
+          </div>
+
+          {/* image */}
+          <div className="sinlge-input-part image">
+            <label htmlFor="image">Image</label>
+            <input
+              className="clear_after"
+              type="url"
+              required
+              id="image"
+              ref={imageInputRef}
+            />{" "}
+            <br />
+          </div>
+
+          {/* address */}
+          <div className="sinlge-input-part address">
+            <label htmlFor="address">Address</label>
+            <input
+              className="clear_after"
+              type="text"
+              required
+              id="address"
+              ref={addressInputRef}
+            />
+            <br />
+          </div>
+
+          {/* description */}
+          <div className="sinlge-input-part description">
+            <label htmlFor="description">Desctiption</label>
+            <textarea
+              className="clear_after"
+              name="description"
+              id="description"
+              required
+              rows={5}
+              ref={descriptionInputRef}
+            ></textarea>
+            <br />
+          </div>
+          {/* button */}
+          <div className="button-div">
+            <button className="button">Add New Product</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default AddNewForm;
